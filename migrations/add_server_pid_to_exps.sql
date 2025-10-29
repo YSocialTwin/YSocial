071b96ff8915ddc348affb4b02f6e6ba9ef1b963
GiulioRossetti <giulio.rossetti@gmail.com>
2025-10-29 15:00:45 +0100

    :memo: docs

--- migrations/add_server_pid_to_exps.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Add server_pid column to exps table
--- Date: 2025-10-29
--- Description: Adds server_pid column to track server process IDs for graceful termination
-
--- For SQLite
--- ALTER TABLE exps ADD COLUMN server_pid INTEGER DEFAULT NULL;
-
--- For PostgreSQL
-ALTER TABLE exps ADD COLUMN IF NOT EXISTS server_pid INTEGER DEFAULT NULL;
